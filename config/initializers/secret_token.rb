80df369b09c53b6460705392618e144881e908f0
shimesaba-type0 <shimesaba@shimesaba-top.(none)>
2013-07-08 22:46:57 +0900

    初めてのcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '4ded300d856e332f22fcb5c2bf35c8ea2b04b944b17177a136a5075a3bd38227744ff55d8a3fc74209f2f179823611ed21e51a8a4fac685d0df5b4c0a5ea7de0'
