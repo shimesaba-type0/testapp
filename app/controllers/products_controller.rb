9e20648210b326504f88c5fa3054d87200e021fb
shimesaba-type0 <shimesaba@shimesaba-top.(none)>
2013-07-10 00:06:46 +0900

    csvダウンロード可能になった

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.order(:name)
+
+    respond_to do |format|
+      format.html
+      format.csv { render text: @products.to_csv }
+    end
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Product.create!(name: "eva1", released_on: rand(1..90).days.ago)
+Product.create!(name: "eva2", released_on: rand(1..90).days.ago)
+Product.create!(name: "eva3", released_on: rand(1..90).days.ago)
