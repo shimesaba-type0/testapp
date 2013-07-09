5c7fae8abe2508c08570e9d66edcb645b6166794
shimesaba-type0 <shimesaba@shimesaba-top.(none)>
2013-07-10 00:27:11 +0900

    xlsフォーマット指定バージョンを作成。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,7 @@ class ProductsController < ApplicationController
       format.html
       # format.csv { render text: @products.to_csv }
       format.csv { send_data @products.to_csv }
-      format.xls { send_data @products.to_csv(col_sep: "\t") }
-      # format.xlsx { send_data @products.to_csv(col_sep: "\t") }
+      format.xls
     end
   end
 
